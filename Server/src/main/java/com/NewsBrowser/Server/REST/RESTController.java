9ae6d818b38f564fa38e5fa86c515f5df18e5611
mjazy <112projects.contact@gmail.com>
2018-10-29 23:08:11 +0100

    29th commit: added @CrossOrigin annotation in RESTController of Server module

--- Server/src/main/java/com/NewsBrowser/Server/REST/RESTController.java
+++ Server/src/main/java/com/NewsBrowser/Server/REST/RESTController.java
@@ -2,6 +2,7 @@ package com.NewsBrowser.Server.REST;
 
 import javax.inject.Inject;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +31,12 @@ public class RESTController {
 	
 	/**
 	 * Controller for NewsAPI news fetching process.
+	 * Value of origins is of Client module.
 	 * @param country should be lower case ISO 3166-1 code. At this moment only "pl" is supported by this application.
 	 * @param category should be one of "business, entertainment, general, health, science, sports, technology". 
 	 * @return News in form compliant with requirements or null in case input is not valid.
 	 */
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/news/{country}/{category}/")
 	News newsAPIFetchNews(@PathVariable String country, @PathVariable String category) {
 		
